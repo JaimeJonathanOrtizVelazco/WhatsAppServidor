b2e8a347484d0b340cc868f1eb6991b3fbee2058
Jaime Jonathan Ortiz Velazco <jonathan.ortiz.velazco@gmail.com>
2021-01-11 14:29:12 -0600

    Server sockets ack

--- main.c
+++ main.c
@@ -14,15 +14,18 @@
 pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
 char username[TAM_BUFFER];
 char password[TAM_BUFFER];
-pthread_mutex_t sendMutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *client_menu(void *accept_sd);
+void *client(void *accept_sd);
+
+int LoginRegisterMenu(void *accept_sd);
+
+int LoggedUserMenu(void *accept_sd);
+
+int VerifyOrRegisterUser(void *cliente_sockfd, int option);
 
 void SendToSocket(void *accept_sd, void *bufferMessage);
 
 void RecievFSocket(void *accept_sd, void *variable);
-
-int VerifyOrRegisterUser(void *cliente_sockfd, void *File, int option);
 
 int main() {
     int listen_sd, accept_sd;
@@ -46,51 +49,71 @@ int main() {
     while (1) {
         if ((accept_sd = accept(listen_sd, NULL, NULL)) < 0) {
             close(listen_sd);
-            exit(-1);
         }
-        if (pthread_create(&id_hilo, NULL, client_menu, (void *) &accept_sd) != 0) {
+        if (pthread_create(&id_hilo, NULL, client, (void *) &accept_sd) != 0) {
             close(accept_sd);
-            exit(-1);
         }
     }
 }
 
-void *client_menu(void *accept_sd) {
+void *client(void *accept_sd) {
+    if (LoginRegisterMenu(&*(int *) accept_sd) != 1) {
+        close(*((int *) accept_sd));
+        pthread_exit(NULL);
+    }
+
+}
+int LoggedUserMenu(void *accept_sd){
     char buffermessage[TAM_BUFFER];
-    FILE *file;
-    int flag = 0;
-    while (flag == 0) {
-        SendToSocket(&*(int *) accept_sd, "Que desea realizar?");
+    while (1) {
+        SendToSocket(&*(int *) accept_sd, );
+        SendToSocket(&*(int *) accept_sd, "1)Mis contactos");
+        SendToSocket(&*(int *) accept_sd, "2)Cambiar estatus");
+        SendToSocket(&*(int *) accept_sd, "3)Cerrar sesion");
+        RecievFSocket(&*(int *) accept_sd, &buffermessage);
+        int caseOption = atoi(buffermessage);
+        if (caseOption == 3) {
+            SendToSocket(&*((int *) accept_sd), "quit()");
+            return 0;
+        }
+        if (VerifyOrRegisterUser(&*((int *) accept_sd), caseOption) == 1) {
+            if (caseOption == 1)
+                SendToSocket(&*((int *) accept_sd), "Se ha logeado exitosamente");
+            else
+                SendToSocket(&*((int *) accept_sd), "Se ha registrado exitosamente");
+            return 1;
+        } else {
+            if (caseOption == 1)
+                SendToSocket(&*((int *) accept_sd), "Usuario o contrasena erroneos");
+            else
+                SendToSocket(&*((int *) accept_sd), "Usuario ya existe, intente registrar uno nuevo");
+        }
+    }
+}
+int LoginRegisterMenu(void *accept_sd) {
+    char buffermessage[TAM_BUFFER];
+    while (1) {
+        SendToSocket(&*(int *) accept_sd, "BIENVENIDO");
         SendToSocket(&*(int *) accept_sd, "1)Iniciar Sesion");
         SendToSocket(&*(int *) accept_sd, "2)Registrarse");
         SendToSocket(&*(int *) accept_sd, "3)Terminar la aplicacion");
         RecievFSocket(&*(int *) accept_sd, &buffermessage);
         int caseOption = atoi(buffermessage);
-        switch (caseOption) {
-            case 1:
-            case 2:
-                if (VerifyOrRegisterUser(&*((int *) accept_sd), &file, caseOption)) {
-                    if (caseOption == 1)
-                        SendToSocket(&*((int *) accept_sd), "Se ha logeado exitosamente");
-                    else
-                        SendToSocket(&*((int *) accept_sd), "Se ha registrado exitosamente");
-                } else {
-                    if (caseOption == 1)
-                        SendToSocket(&*((int *) accept_sd), "Usuario o contrasena erroneos");
-                    else
-                        SendToSocket(&*((int *) accept_sd), "Usuario ya existe, intente registrar uno nuevo");
-                }
-                break;
-            case 3:
-                flag = 1;
-                break;
-            default:
-                break;
+        if (caseOption == 3)
+            return 0;
+        if (VerifyOrRegisterUser(&*((int *) accept_sd), caseOption) == 1) {
+            if (caseOption == 1)
+                SendToSocket(&*((int *) accept_sd), "Se ha logeado exitosamente");
+            else
+                SendToSocket(&*((int *) accept_sd), "Se ha registrado exitosamente");
+            return 1;
+        } else {
+            if (caseOption == 1)
+                SendToSocket(&*((int *) accept_sd), "Usuario o contrasena erroneos");
+            else
+                SendToSocket(&*((int *) accept_sd), "Usuario ya existe, intente registrar uno nuevo");
         }
     }
-    SendToSocket(&*((int *) accept_sd), "quit()");
-    close(*((int *) accept_sd));
-    pthread_exit(NULL);
 }
 
 void RecievFSocket(void *accept_sd, void *variable) {
@@ -102,18 +125,52 @@ void RecievFSocket(void *accept_sd, void *variable) {
 }
 
 void SendToSocket(void *accept_sd, void *bufferMessage) {
-    int rc;
-    pthread_mutex_lock(&sendMutex);
-    if ((rc = send(*(int *) accept_sd, (char *) bufferMessage, strlen((char *) bufferMessage), 0)) < 0) {
-        close(*(int *) accept_sd);
-        pthread_exit(NULL);
+    char ok[TAM_BUFFER];
+    int lgData;
+    while (1) {
+        if ((send(*(int *) accept_sd, "SEND", strlen((char *) bufferMessage), 0)) < 0) {
+            close(*(int *) accept_sd);
+            pthread_exit(NULL);
+        }
+        if ((lgData = recv(*(int *) accept_sd, &ok, TAM_BUFFER, 0)) < 0) {
+            close(*(int *) accept_sd);
+            pthread_exit(NULL);
+        }
+        if (lgData == strlen((char *) bufferMessage) && strcmp(ok, "OK") == 0) {
+            if ((send(*(int *) accept_sd, "OK", TAM_BUFFER, 0)) < 0) {
+                close(*(int *) accept_sd);
+                pthread_exit(NULL);
+            }
+        } else {
+            if ((send(*(int *) accept_sd, "NO", TAM_BUFFER, 0)) < 0) {
+                close(*(int *) accept_sd);
+                pthread_exit(NULL);
+            }
+            sleep(1);
+            continue;
+        }
+        if ((recv(*(int *) accept_sd, &ok, TAM_BUFFER, 0)) < 0) {
+            close(*(int *) accept_sd);
+            pthread_exit(NULL);
+        }
+        if ((send(*(int *) accept_sd, (char *) bufferMessage, strlen((char *) bufferMessage), 0)) < 0) {
+            close(*(int *) accept_sd);
+            pthread_exit(NULL);
+        }
+        if ((recv(*(int *) accept_sd, &ok, TAM_BUFFER, 0)) < 0) {
+            close(*(int *) accept_sd);
+            pthread_exit(NULL);
+        }
+        if (strcmp(ok, "OK") != 0) {
+            sleep(1);
+            continue;
+        }
+        break;
     }
-    sleep(1);
-    if (rc == strlen((char *) bufferMessage))
-        pthread_mutex_unlock(&sendMutex);
 }
 
-int VerifyOrRegisterUser(void *cliente_sockfd, void *File, int option) {
+int VerifyOrRegisterUser(void *cliente_sockfd, int option) {
+    FILE *File;
     char registry[TAM_BUFFER];
     SendToSocket(&*(int *) cliente_sockfd, "Nombre de usuario: ");
     RecievFSocket(&*(int *) cliente_sockfd, &username);
